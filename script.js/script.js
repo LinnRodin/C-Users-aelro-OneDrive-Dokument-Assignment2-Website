81242770804a6993d543c7a2970d5ece3aaa7583
Linn Rodin Sjögren <112092425+LinnRodin@users.noreply.github.com>
2022-10-17 15:48:24 +0200

    updated with validation

--- script.js/script.js
+++ script.js/script.js
@@ -1,3 +1,25 @@
+const checkLength = (element, ErrorMessage, minLength = 2) => {
+    console.log(element.target.id)
+
+    if (element.target.value.length < minLength) {
+    document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "You must enter a name"
+    } else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
+    }
+}
+
 const validate = (e) => {
-    console.log(e.target.type)
+    switch(e.target.type) {
+        case "text":
+            checkLength(e)
+            break;
+        case "email":
+            break;
+        case "textarea":
+            checkLength(e, 5)
+            break;
+    }
+
 }
